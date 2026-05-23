6fc640a12cb490e02ea2c747d047b5ac0ba59502
NaviGamage <navinduprabashwara@gmail.com.com>
2026-05-23 15:09:08 +0530

    feat: add validation to SignupRequest DTO

--- src/main/java/com/library/dto/SignupRequest.java
+++ src/main/java/com/library/dto/SignupRequest.java
@@ -1,11 +1,24 @@
 package com.library.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SignupRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must not exceed 100 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
     private String role;
 }
